164fb06cf6dca7e15b4c23b6df40418504818075
edmar-afk <jaywrsnp6@gmail.com>
2025-12-02 12:09:46 +0800

    new

--- src/components/modals/TransportMapModal.jsx
+++ src/components/modals/TransportMapModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Modal, Box, Button } from "@mui/material";
+import { Modal, Box } from "@mui/material";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -47,6 +47,14 @@ function TransportMapModal({ transportId }) {
       iconAnchor: [15, 30],
     });
 
+  const currentCoords = transportation?.current_location
+    ? transportation.current_location.split(",").map(Number)
+    : null;
+
+  const destinationCoords = transportation?.destination
+    ? transportation.destination.split(",").map(Number)
+    : null;
+
   return (
     <>
       <p onClick={handleOpen} sx={{ mt: 2 }}>
@@ -55,7 +63,6 @@ function TransportMapModal({ transportId }) {
 
       <Modal open={open} onClose={handleClose} sx={{ zIndex: 99999 }}>
         <Box sx={style} className="relative">
-          {/* Close button */}
           <button
             onClick={handleClose}
             className="absolute z-[9999] top-2 right-2 bg-red-500 text-white px-3 py-1.5 rounded-full font-bold shadow-md hover:bg-red-600 active:scale-95 transition"
@@ -65,9 +72,7 @@ function TransportMapModal({ transportId }) {
 
           {transportation ? (
             <MapContainer
-              center={transportation.current_location
-                .split(",")
-                .map((n) => parseFloat(n))}
+              center={currentCoords || [0, 0]}
               zoom={16}
               style={{ height: "400px", width: "100%", borderRadius: "8px" }}
               attributionControl={false}
@@ -75,19 +80,19 @@ function TransportMapModal({ transportId }) {
             >
               <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
 
-              <Marker
-                position={transportation.current_location
-                  .split(",")
-                  .map((n) => parseFloat(n))}
-                icon={labelIcon("You", "text-blue-600")}
-              />
+              {currentCoords && (
+                <Marker
+                  position={currentCoords}
+                  icon={labelIcon("You", "text-blue-600")}
+                />
+              )}
 
-              <Marker
-                position={transportation.destination
-                  .split(",")
-                  .map((n) => parseFloat(n))}
-                icon={labelIcon("Destination", "text-red-600")}
-              />
+              {destinationCoords && (
+                <Marker
+                  position={destinationCoords}
+                  icon={labelIcon("Destination", "text-red-600")}
+                />
+              )}
             </MapContainer>
           ) : (
             <p>Loading map...</p>
